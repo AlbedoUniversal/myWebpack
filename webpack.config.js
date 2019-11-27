818c2b7ab3047e41aee208c1825f5fb141ad314b
AlbedoUniversal <Albedogforce@gmail.com>
2019-11-27 13:38:51 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,16 @@ module.exports = {
   },
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./dist"),
     publicPath: "/dist"
   },
+  module: { 
+    rules: [{ //правила
+      test: /\.js$/, //регулярка на то, что мы будем проверять
+      loader: 'babel-lodar' //обратились через легулярку ко всем js файлам
+      exclude: '/node_moduloes' //чтобы ускорить нашу компиляцию, мы будем исключать какие-либо файлы
+    }]
+  }
   // plugins: [
   //   new HtmlWebpackPlugin({
   //     filename: "index.html",
